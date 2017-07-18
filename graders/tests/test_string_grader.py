60759b6010b939e6c43cf5206b223a0af3b35a40
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-07-17 23:27:27 -0400

    add some StringGrader tests

---
+++ graders/tests/test_string_grader.py
@@ -0,0 +1,43 @@
+from __future__ import division
+from .. import graders
+
+def test_single_expect_value_in_config():
+    grader = graders.StringGrader({
+        'expect':'cat'
+    })
+    submission = 'dog'
+    expected_result = {'msg': '', 'grade_decimal': 0, 'ok': False}
+    assert grader.cfn(None, submission) == expected_result
+
+def test_list_of_expect_values():
+    grader = graders.StringGrader({
+        'expect':[
+            {'expect':'zebra', 'grade_decimal':1},
+            {'expect':'horse', 'grade_decimal':0.45},
+            {'expect':'unicorn', 'grade_decimal':0, 'msg': "unicorn_msg"}
+        ]
+    })
+    submission = 'horse'
+    expected_result = {'msg': '', 'grade_decimal': 0.45, 'ok': 'partial'}
+    assert grader.cfn(None, submission) == expected_result
+    
+def test_longer_message_wins_grade_ties():
+    grader1 = graders.StringGrader({
+        'expect':[
+            {'expect':'zebra', 'grade_decimal':1},
+            {'expect':'horse', 'grade_decimal':0, 'msg':'short'},
+            {'expect':'unicorn', 'grade_decimal':0, 'msg': "longer_msg"}
+        ]
+    })
+    grader2 = graders.StringGrader({
+        'expect':[
+            {'expect':'zebra', 'grade_decimal':1},
+            {'expect':'unicorn', 'grade_decimal':0, 'msg': "longer_msg"},
+            {'expect':'horse', 'grade_decimal':0, 'msg':'short'}
+        ]
+    })
+    submission = 'unicorn'
+    expected_result = {'msg': 'longer_msg', 'grade_decimal': 0, 'ok': False}
+    result1 = grader1.cfn(None, submission)
+    result2 = grader2.cfn(None, submission)
+    assert result1 == result2 == expected_result
