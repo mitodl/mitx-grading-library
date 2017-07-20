835adf4dd18efd8a0933bd8c8cc64f94db92013e
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-07-20 09:57:00 -0400

    add FormulaGrader placeholder

--- graders/graders.py
+++ graders/graders.py
@@ -290,3 +290,6 @@ class StringGrader(ItemGrader):
             return {'ok':answer['ok'], 'grade_decimal':answer['grade_decimal'], 'msg':answer['msg']}
         else:
             return {'ok':False, 'grade_decimal':0, 'msg':''}
+
+class FormulaGrader(ItemGrader):
+    pass
