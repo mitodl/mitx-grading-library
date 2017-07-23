933aae9e16331913a6d6e403e0d8911b450bea49
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-07-23 14:46:18 -0400

    Add configuration description to FormulaGrader docstring

--- graders/formulagrader/formulagrader.py
+++ graders/formulagrader/formulagrader.py
@@ -192,21 +192,6 @@ class FormulaGrader(NumericalGrader):
     Similar to EdX formularesponse but more flexible. Allows author
     to specify functions in addition to variables.
     
-    FormulaGrader({
-        'answers':['a+b^2'],
-        'variables': ['a', 'b', 'c'],
-        'functions': ['f', 'g', 'h'],
-        'sample_from': {
-            'a': [1,3],
-            'b': [1,3],
-            'c': RealInterval(1, 3),
-            'd': IntegerInterval(-5,5),
-            'd': MatrixGroupSO(2),
-            'h': PolynomialFunction
-        },
-        'samples':5
-    })
-    
     Usage
     =====
     
@@ -239,6 +224,24 @@ class FormulaGrader(NumericalGrader):
     >>> input0 = "b*b - 0.25*f(g(a))"
     >>> grader.cfn(None, input0)['ok']
     True
+    
+    Configuration Dictionary Keys
+    =============================
+    
+    answers (list): answers, each specified as a string or dictionary.
+    variables (list of str): variable names, default []
+    functions (list of str): function names, default []
+    samples (int): Positive number of samples to use, default 5
+    sample_from: A dictionary mapping synbol (variable or function name) to
+        sampling sets. Default sampling sets are:
+            for variables, RealInterval([1,3])
+            for functions, NiceFunctions({dims=[1,1]})
+    tolerance (int or PercentageString): A positive tolerance with which to 
+        compare numerical evaluations. Default '0.1%'
+    case_sensitive (bool): whether symbol names are case senstive. Default True
+    failable_evals (int): The nonnegative maximum number of evaluation
+         comparisons that can fail with grader still passing. Default 0
+    
     """
     
     def validate_input(self, value):
