b004519fcf5c8dfdfb030f7dab0c69857652bf3b
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-07-22 23:52:46 -0400

    add doctests for NiceFunctions and RealInterval

--- graders/formulagrader/formulagrader.py
+++ graders/formulagrader/formulagrader.py
@@ -9,7 +9,26 @@ class AbstractSamplingSet(ObjectWithSchema):
     pass
 
 class RealInterval(AbstractSamplingSet):
-    """Represents an interval of real numbers from which to sample."""
+    """Represents an interval of real numbers from which to sample.
+    
+    Usage
+    =====
+    
+    Generate 5 random floats betweens -2 and 4
+    >>> ri = RealInterval({'start':-2, 'stop':4})
+    >>> ri.gen_samples(5) # doctest: +SKIP
+    [ 2.44247 -0.67699 -1.36759 -0.11255  1.39864]
+    
+    You can initialize with a pair instead of a dict:
+    >>> ri = RealInterval([-2,4])
+    >>> ri.gen_samples(5) # doctest: +SKIP
+    [ 2.9973   2.95767  0.069    0.23813 -1.49541]
+    
+    The default is {'start':1, 'stop':3}:
+    >>> ri = RealInterval()
+    >>> ri.gen_samples(5) # doctest: +SKIP
+    [ 2.61484  1.38107  2.61687  1.00507  1.87933]    
+    """
     
     def standardize_alternate_config(config_as_list):
         alternate_form = Schema(All(
@@ -38,10 +57,32 @@ class RealInterval(AbstractSamplingSet):
 
 class NiceFunctions(AbstractSamplingSet):
     """Represents space of 'nice' functions from which to sample.
+    
+    Usage
+    =====
+    
+    Generate a random continous function:
+    >>> funcs = NiceFunctions()
+    >>> f = funcs.gen_sample()
+    >>> [f(1.2), f(1.2), f(1.3), f(4)] # doctest: +SKIP
+    [-1.89324 -1.89324 -2.10722  0.85814]
+    
+    By default, the generated functions are R-->R. You can specify the
+    input and output dimensions:
+    >>> funcs = NiceFunctions({'dims':[3,2]})
+    >>> f = funcs.gen_sample()
+    >>> f(2.3, -1, 4.2) # doctest: +SKIP
+    [-1.74656 -0.96909]
+    >>> f(2.3, -1.1, 4.2) # doctest: +SKIP
+    [-1.88769 -1.32087]
+    
+    NOTE:
+        Currently implemented as a sum of trigonometric functions
+        with random phases and periods.
     """
     
     schema_config = Schema({
-        Required('dims', default=1): All(
+        Required('dims', default=[1,1]): All(
             list,
             Length(min=2, max=2),
             [All(int, Range(0,float('inf')))]
