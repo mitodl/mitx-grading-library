cfc0427c67156e1df2eb0a9d63ea78425e6e2561
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-07-23 14:28:25 -0400

    add Positive and NonNegative

--- graders/validatorfuncs.py
+++ graders/validatorfuncs.py
@@ -1,11 +1,22 @@
 """Standalone validator functions for use in voluptuous Schema."""
 
+from voluptuous import All, Range, NotIn
+
+def Positive(thetype):
+    if thetype==int:
+        return All(thetype, Range(1, float('inf')))
+    else:
+        return All(thetype, Range(0, float('inf')), NotIn([0]))
+
+def NonNegative(thetype):
+    return All(thetype, Range(0, float('inf')))
+
 def PercentageString(value):
     """Validate that a string can be interpretted as a percentage."""
     if isinstance(value, str) and value.endswith("%"):
         try:
             percent = float(value[:-1])
-            return percent
+            return "{percent}%".format(percent=percent)
         except:
             pass
     
