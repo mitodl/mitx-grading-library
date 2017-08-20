bf3c52f9b8902122cec0adf3d4b024aaf56c1813
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-08-20 14:18:07 -0400

    remove a newline

--- basic_demo.py
+++ basic_demo.py
@@ -5,9 +5,9 @@ pp = pprint.PrettyPrinter(indent=4)
 
 list_grader = ListGrader({
     'answers_list': [['cat'], ['dog'], ['unicorn']],
-    'item_grader': StringGrader() 
+    'item_grader': StringGrader()
 })
- 
+
 answers1 = ['cat', 'fish', 'dog']
 demo1 = list_grader.cfn(None, answers1)
 pp.pprint(demo1)
