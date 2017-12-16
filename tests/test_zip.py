7d9e80bcd531f813601c3cfa191c142be918d2fe
Chris Chudzicki <christopher.chudzicki@gmail.com>
2017-12-16 09:57:22 -0500

    use test_zip.py's path to locate python_lib.zip

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -11,7 +11,9 @@ def loadzip():
     """pytest fixture to dynamically load the library from python_lib.zip"""
     # Add python_lib.zip to the path for searching for modules
     import sys
-    sys.path.insert(0, '/Users/jolyon/gitrepos/mitx-graders/mitx-graders/python_lib.zip')
+    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    zip_path = os.path.join(parent_dir, 'python_lib.zip')
+    sys.path.insert(0, zip_path)
     # The 0 tells it to search this zip file before even the module directory
     # Hence,
     reload(mitxgraders)
