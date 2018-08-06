d63b032dd52f3a5844915e326ad57c37c81023bc
Chris Chudzicki <christopher.chudzicki@gmail.com>
2018-08-06 15:07:22 -0400

    name updateMathJax function

--- MJxPrep.js
+++ MJxPrep.js
@@ -45,156 +45,155 @@ if (window.MJxPrep) {
     };
   };
 
+  function updateMathJax() {
+    if (MathJax.InputJax.AsciiMath) {
+      // Grab the AsciiMath object
+      AM = MathJax.InputJax.AsciiMath.AM;
+
+      // All of these new symbols are based on those appearing in the AsciiMath definitions
+      // See https://github.com/asciimath/asciimathml/blob/master/ASCIIMathML.js
+      // Add functions, including some edX functions that don't exist in AsciiMath
+      AM.newsymbol({
+        input: "arcsec",
+        tag: "mi",
+        output: "arcsec",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arccsc",
+        tag: "mi",
+        output: "arccsc",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arccot",
+        tag: "mi",
+        output: "arccot",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arcsinh",
+        tag: "mi",
+        output: "arcsinh",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arccosh",
+        tag: "mi",
+        output: "arccosh",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arctanh",
+        tag: "mi",
+        output: "arctanh",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arcsech",
+        tag: "mi",
+        output: "arcsech",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arccsch",
+        tag: "mi",
+        output: "arccsch",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "arccoth",
+        tag: "mi",
+        output: "arccoth",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "re",
+        tag: "mi",
+        output: "Re",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "im",
+        tag: "mi",
+        output: "Im",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+      AM.newsymbol({
+        input: "conj",
+        tag: "mover",
+        output: "\xAF",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        acc: true
+      });
+      AM.newsymbol({
+        input: "trace",
+        tag: "mi",
+        output: "Tr",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        func: true
+      });
+
+      // Add special functions: fact and factorial
+      AM.newsymbol({
+        input: "fact",
+        tag: "mo",
+        output: "fact",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        rewriteleftright: [ "", "!" ]
+      });
+      AM.newsymbol({
+        input: "factorial",
+        tag: "mo",
+        output: "factorial",
+        tex: null,
+        ttype: AM.TOKEN.UNARY,
+        rewriteleftright: [ "", "!" ]
+      });
+
+      // Ask MathJax to reprocess all input boxes, as saved answers may have rendered
+      // before these definitions went through
+      MathJax.Hub.Queue([
+        "Reprocess",
+        MathJax.Hub,
+        "div.equation"
+      ]);
+
+      // No need to update again
+      clearInterval(checkExist);
+      console.log(
+        "MITx Grading Library: Updated AsciiMath renderer definitions"
+      );
+    }
+  }
+
   // Try to update AsciiMath
-  var checkExist = setInterval(
-    function() {
-      if (MathJax.InputJax.AsciiMath) {
-        // Grab the AsciiMath object
-        AM = MathJax.InputJax.AsciiMath.AM;
-
-        // All of these new symbols are based on those appearing in the AsciiMath definitions
-        // See https://github.com/asciimath/asciimathml/blob/master/ASCIIMathML.js
-        // Add functions, including some edX functions that don't exist in AsciiMath
-        AM.newsymbol({
-          input: "arcsec",
-          tag: "mi",
-          output: "arcsec",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arccsc",
-          tag: "mi",
-          output: "arccsc",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arccot",
-          tag: "mi",
-          output: "arccot",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arcsinh",
-          tag: "mi",
-          output: "arcsinh",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arccosh",
-          tag: "mi",
-          output: "arccosh",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arctanh",
-          tag: "mi",
-          output: "arctanh",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arcsech",
-          tag: "mi",
-          output: "arcsech",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arccsch",
-          tag: "mi",
-          output: "arccsch",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "arccoth",
-          tag: "mi",
-          output: "arccoth",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "re",
-          tag: "mi",
-          output: "Re",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "im",
-          tag: "mi",
-          output: "Im",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-        AM.newsymbol({
-          input: "conj",
-          tag: "mover",
-          output: "\xAF",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          acc: true
-        });
-        AM.newsymbol({
-          input: "trace",
-          tag: "mi",
-          output: "Tr",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          func: true
-        });
-
-        // Add special functions: fact and factorial
-        AM.newsymbol({
-          input: "fact",
-          tag: "mo",
-          output: "fact",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          rewriteleftright: [ "", "!" ]
-        });
-        AM.newsymbol({
-          input: "factorial",
-          tag: "mo",
-          output: "factorial",
-          tex: null,
-          ttype: AM.TOKEN.UNARY,
-          rewriteleftright: [ "", "!" ]
-        });
-
-        // Ask MathJax to reprocess all input boxes, as saved answers may have rendered
-        // before these definitions went through
-        MathJax.Hub.Queue([
-          "Reprocess",
-          MathJax.Hub,
-          "div.equation"
-        ]);
-
-        // No need to update again
-        clearInterval(checkExist);
-        console.log(
-          "MITx Grading Library: Updated AsciiMath renderer definitions"
-        );
-      }
-    },
-    200
-  );
+  var checkExist = setInterval(200);
   // Check for the AsciiMath object every 200ms
   function findClosingBrace(expr, startIdx) {
     var braces = { "[": "]", "<": ">", "(": ")", "{": "}" };
